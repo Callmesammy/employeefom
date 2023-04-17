b04852b19efae36038ac6b917271cd7d4af762f2
HP <callmesammy@yahoo.com>
2023-04-17 20:43:02 +0100

    File

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
 import Employee from './Employee';
 import Navbar from './Navbar';
+import EmployeeList from '../EmployeeList';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
     <Navbar/>
-    <Employee/>
+    <Routes>
+      <Routes index element = {<EmployeeList/>}></Routes>
+      <Routes path= "/" element={<EmployeeList/>}></Routes>
+      <Routes path= "employeeList" element={<EmployeeList/>}></Routes>
+      <Routes path= "employee" element={<Employee/>}></Routes>
+    </Routes>
+    </BrowserRouter>
+    
     </>
   );
   
